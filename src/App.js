0ba967c39dcba11c03b42a16ef5cf53a3446a64e
Chenemi Zekeri <chenemi2@gmail.com>
2020-11-18 08:31:15 +0100

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import Homepage from './Pages/Homepage';
-//import EmergencyPage from './Pages/EmergencyType';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './Pages/Homepage';
+import EmergencyPage from './Pages/EmergencyType';
+import LocationPage from './Pages/LocationPage';
+import LoginPage from './Pages/LoginPage';
+import SignupPage from './Pages/SignupPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Homepage/>
-    </div>
+    <main>
+      <Switch>
+        <Route path="/login" component={LoginPage} exact />
+        <Route path="/signup" component={SignupPage} exact />
+        <Route path="/" component={HomePage} exact />
+        <Route path="/emergency" component={EmergencyPage} />
+        <Route path="/location" component={LocationPage} />
+        <Route component={Error} />
+      </Switch>
+    </main>
   );
 }
 
